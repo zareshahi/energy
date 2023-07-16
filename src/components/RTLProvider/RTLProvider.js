3efeed3beffbac919935ddee308d3e214469bf5a
Ali Zareshahi <a.zareshahi1377@gmail.com>
2023-07-16 21:39:20 +0330

    rtl provider component ar replaced with fa

--- src/components/RTLProvider/RTLProvider.js
+++ src/components/RTLProvider/RTLProvider.js
@@ -3,12 +3,12 @@ import createCache from "@emotion/cache";
 import rtl from "stylis-plugin-rtl";
 // NB: A unique `key` is important for it to work!
 const options = {
-    rtl: {key: "css-ar", stylisPlugins: [rtl]},
+    rtl: {key: "css-fa", stylisPlugins: [rtl]},
     ltr: {key: "css-en"},
 };
 
 export function RtlProvider({children}) {
-    const dir = document.documentElement.dir == "ar" ? "rtl" : "ltr";
+    const dir = document.documentElement.dir == "fa" ? "rtl" : "ltr";
     const cache = createCache(options[dir]);
     return <CacheProvider value={cache} children={children}/>;
 }
